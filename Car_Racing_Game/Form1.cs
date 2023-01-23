0358aee47dd00427a257badba95cdd326fec5a84
rushi-smb <rushi@smbios.co.in>
2023-01-23 22:57:45 +0530

    Car Racing Game First Commit

---
+++ Car_Racing_Game/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Car_Racing_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            over.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void moveline(int speed)
+        {
+            if (pictureBox1.Top >= 500) { pictureBox1.Top = 0; } else { pictureBox1.Top += speed; }
+
+            if (pictureBox2.Top >= 500) { pictureBox2.Top = 0; } else { pictureBox2.Top += speed; }
+
+            if (pictureBox3.Top >= 500) { pictureBox3.Top = 0; } else { pictureBox3.Top += speed; }
+
+            if (pictureBox4.Top >= 500) { pictureBox4.Top = 0; } else { pictureBox4.Top += speed; }
+
+            if (pictureBox5.Top >= 500) { pictureBox5.Top = 0; } else { pictureBox5.Top += speed; }
+
+            pictureBox1.Top += speed;
+            pictureBox2.Top += speed;
+            pictureBox3.Top += speed;
+            pictureBox4.Top += speed;
+            pictureBox5.Top += speed;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveline(gamespeed);
+            enemy(gamespeed);
+            gameover();
+            coins(gamespeed);
+            coinscollection();
+        }
+
+        int collectedcoin = 0;
+
+        Random r = new Random();
+        int x, y;
+        void enemy(int speed)
+        {
+            if (enemy1.Top >= 500){ x = r.Next(0, 380);enemy1.Location = new Point(x, 0);}else { enemy1.Top += speed; }
+
+            if (enemy2.Top >= 500){ x = r.Next(0, 380);enemy2.Location = new Point(x, 0);}else { enemy2.Top += speed; }
+
+            if (enemy3.Top >= 500){ x = r.Next(0, 380); enemy3.Location = new Point(x, 0);}else{ enemy3.Top += speed; }
+        }
+        void coins(int speed)
+        {
+            if (coin1.Top >= 500){ x = r.Next(0, 200); coin1.Location = new Point(x, 0); }else { coin1.Top += speed; }
+
+            if (coin2.Top >= 500){ x = r.Next(0, 200); coin2.Location = new Point(x, 0); }else { coin2.Top += speed; }
+
+            if (coin3.Top >= 500){ x = r.Next(0, 380); coin3.Location = new Point(x, 0); }else { coin3.Top += speed; }
+
+            if (coin4.Top >= 500){ x = r.Next(0, 380); coin4.Location = new Point(x, 0); }else { coin4.Top += speed; }
+        }
+        void gameover()
+        {
+            if (car.Bounds.IntersectsWith(enemy1.Bounds)) {timer1.Enabled = false; over.Visible = true; }
+
+            if (car.Bounds.IntersectsWith(enemy2.Bounds)) { timer1.Enabled = false;over.Visible = true; }
+
+            if (car.Bounds.IntersectsWith(enemy3.Bounds)) { timer1.Enabled = false;over.Visible = true; }
+        }
+        
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void coinscollection()
+        {
+            if (car.Bounds.IntersectsWith(coin1.Bounds)) { collectedcoin++; label1.Text = "Coins=" + collectedcoin.ToString(); x = r.Next(0, 380); coin1.Location = new Point(x, 0);}
+            if (car.Bounds.IntersectsWith(coin2.Bounds)) { collectedcoin++; label1.Text = "Coins=" + collectedcoin.ToString(); x = r.Next(0, 380); coin2.Location = new Point(x, 0); }
+            if (car.Bounds.IntersectsWith(coin3.Bounds)) { collectedcoin++; label1.Text = "Coins=" + collectedcoin.ToString(); x = r.Next(0, 380); coin3.Location = new Point(x, 0); }
+            if (car.Bounds.IntersectsWith(coin4.Bounds)) { collectedcoin++; label1.Text = "Coins=" + collectedcoin.ToString(); x = r.Next(0, 380); coin4.Location = new Point(x, 0); }
+        }
+        int gamespeed = 2;  //by default gamespeed
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) { if (car.Left > 0) car.Left += -gamespeed; }
+
+            if (e.KeyCode == Keys.Right) { if (car.Right < 380) car.Left += gamespeed; }
+
+            if (e.KeyCode == Keys.Up) if (gamespeed < 21) { gamespeed++; }
+
+            if (e.KeyCode == Keys.Down) if (gamespeed > 0) { gamespeed--; }
+        }
+    }
+}
